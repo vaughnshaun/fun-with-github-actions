812b8eb22447347058e8a3f0bb7250c25d1906d3
vaughnshaun <vaughnshaun@gmail.com>
2024-02-08 18:45:31 -0500

    add script to authenticate

---
+++ scripts/powerbi.ps1
@@ -0,0 +1,18 @@
+param ($ClientId, $TenantId, $ClientSecret)
+Set-ExecutionPolicy RemoteSigned
+
+Install-Module -Name MicrosoftPowerBIMgmt
+Install-Module -Name MicrosoftPowerBIMgmt.Workspaces
+Install-Module -Name MicrosoftPowerBIMgmt.Data
+Install-Module -Name MicrosoftPowerBIMgmt.Reports
+
+# Create Secure Strings
+
+$SecurePassword = ConvertTo-SecureString $ClientSecret -Force -AsPlainText
+$Credential = New-Object Management.Automation.PSCredential($ClientId, $SecurePassword)
+
+
+# Connect to the Power BI service
+Connect-PowerBIServiceAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential
+
+Set-ExecutionPolicy Default
